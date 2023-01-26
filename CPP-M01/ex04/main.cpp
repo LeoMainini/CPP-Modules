3796202e0b321e3113f21b058d8507e2f748259a
LeoMainini <leonardo.apple.mainini@gmail.com>
2023-01-26 23:22:56 +0000

    update gitignore - implement replace and write to outfile in ex04

--- CPP-M01/ex04/main.cpp
+++ CPP-M01/ex04/main.cpp
@@ -4,20 +4,41 @@
 
 int	main(int argc, char **argv)
 {
-	std::ifstream	file;
-	std::string		text;
-	std::string		line;
-
 	if (argc != 4)
 	{
 		std::cout << "Incorrect arguments" << std::endl;
 		return (0);
 	}
+
+	std::ifstream	file;
+	std::string		text;
+	std::string		line;
+
 	file.open(argv[1]);
 	if (!file.is_open())
+	{
+		std::cout << "File not found" << std::endl;
 		return (1);
+	}
 	while(std::getline(file, line))
 		text.append(line).append("\n");
 	file.close();
-	std::cout << text << std::endl;	
+
+	int	i = 0;
+	unsigned int position = 0;
+	std::string		out_name(argv[1]);
+	std::ofstream	outfile(out_name.append(".replace").c_str());
+	std::string		needle(argv[2]);
+
+	if (!outfile.is_open())
+		std::cout << "File creation failed" << std::endl;
+	while ((position = text.find(needle, i)) < text.length() && outfile.is_open())
+	{
+		outfile << text.substr(i, position - i);
+		outfile << argv[3];
+		i = position + needle.length();
+	}
+	if (i < (int)(text.length() - needle.length()) && outfile.is_open())
+		outfile << &text.c_str()[i + needle.length()];
+	outfile.close();
 }
