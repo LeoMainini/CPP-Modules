e1f8759d03cb5a9cffbfbdfbdc34f3ca1d6e9616
Leo Mainini <leomainini@Leos-MBP.lan>
2023-01-29 13:44:08 +0000

    format ex04

--- CPP-M01/ex04/main.cpp
+++ CPP-M01/ex04/main.cpp
@@ -2,43 +2,51 @@
 #include <fstream>
 #include <sstream>
 
-int	main(int argc, char **argv)
+std::string get_file_text(std::string filename)
 {
-	if (argc != 4)
-	{
-		std::cout << "Incorrect arguments" << std::endl;
-		return (0);
-	}
-
 	std::ifstream	file;
 	std::string		text;
 	std::string		line;
 
-	file.open(argv[1]);
-	if (!file.is_open())
-	{
+	file.open(filename);
+	if (!file.is_open()) {
 		std::cout << "File not found" << std::endl;
-		return (1);
+		exit (1);
 	}
 	while(std::getline(file, line))
 		text.append(line).append("\n");
 	file.close();
+	return (text);
+}
 
-	int	i = 0;
-	unsigned int position = 0;
-	std::string		out_name(argv[1]);
-	std::ofstream	outfile(out_name.append(".replace").c_str());
-	std::string		needle(argv[2]);
+void	replace_text_output(std::string text, std::string filename, std::string needle, std::string replcmnt)
+{
+	int				i = 0;
+	unsigned int	position = 0;
+	std::ofstream	outfile(filename.append(".replace").c_str());
 
 	if (!outfile.is_open())
 		std::cout << "File creation failed" << std::endl;
 	while ((position = text.find(needle, i)) < text.length() && outfile.is_open())
 	{
 		outfile << text.substr(i, position - i);
-		outfile << argv[3];
+		outfile << replcmnt;
 		i = position + needle.length();
 	}
 	if (i < (int)(text.length() - needle.length()) && outfile.is_open())
 		outfile << &text.c_str()[i + needle.length()];
 	outfile.close();
+}
+
+int	main(int argc, char **argv)
+{
+	std::string		text;
+
+	if (argc != 4)
+	{
+		std::cout << "Incorrect arguments" << std::endl;
+		return (0);
+	}
+	text = get_file_text(argv[1]);
+	replace_text_output(text, argv[1], argv[2], argv[3]);
 }
