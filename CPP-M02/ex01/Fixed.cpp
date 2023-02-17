8daa5353c0206be44b784355c2099b7975bd4c8f
Leo Mainini <leomainini@Leos-MBP.lan>
2023-02-17 00:51:05 +0000

    correct implementatios

--- CPP-M02/ex01/Fixed.cpp
+++ CPP-M02/ex01/Fixed.cpp
@@ -7,32 +7,15 @@ Fixed::Fixed( void )
 	std::cout << "Constructor called\n";
 }
 
-Fixed::Fixed( const float num )
+Fixed::Fixed( const float num ) : integral( num * (1 << this->decimal))
 {
-	int i = 1;
-	float value = num;
-
 	std::cout << "Float constructor called\n";
-	while (this->decimal / i > 1)
-	{
-		value *= 10;
-		i *= 2;
-	}
-	this->integral = roundf(value);
 }
 
 Fixed::Fixed( const int num )
 {
-	int i = 1;
-	int	value = num;
-
 	std::cout << "Int constructor called\n";
-	while (this->decimal / i > 1)
-	{
-		value *= 10;
-		i *= 2;
-	}
-	this->integral = value;
+	this->integral = num << 8;
 }
 
 Fixed::~Fixed()
@@ -71,30 +54,11 @@ int	Fixed::getRawBits( void ) const
 
 int	Fixed::toInt( void ) const
 {
-	int	i = 1;
-	int	l_int = getRawBits();
-
-	while (this->decimal / i > 1)
-	{
-		i *= 2;
-		l_int /= 10;
-	}
-	return (l_int);
+	return (this->integral >> this->decimal);
 }
 
 
 float Fixed::toFloat( void ) const
 {
-	int		i = 1;
-	int		l_int = getRawBits();
-	double	result = 0;
-
-	while (this->decimal / i > 1)
-	{
-		result += l_int % 10;
-		l_int /= 10;
-		result /= 10;
-		i *= 2;
-	}
-	return (toInt() + result);
+	return ((this->integral / (float)(1 << this->decimal) ));
 }
