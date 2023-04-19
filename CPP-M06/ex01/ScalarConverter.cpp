82275ce7f809b9dd58768d332005a809cb81eb1e
Leonardo osvaldo Ferreira mainini <leferrei@student.42lisboa.com>
2023-04-19 16:35:52 +0100

    update type finding logic and test range safeties

--- CPP-M06/ex01/ScalarConverter.cpp
+++ CPP-M06/ex01/ScalarConverter.cpp
@@ -1,5 +1,6 @@
 #include "ScalarConverter.hpp"
 #include <stdlib.h>
+#include <errno.h>
 
 using namespace std;
 
@@ -24,16 +25,16 @@ static int		valid_num_str(string &exp)
 {
 	int	not_exponent = 1;
 	//error out on non numeric expression
-	if (exp.find_first_not_of("0123456789.fF-eE+") != string::npos)
+	if (exp.find_first_not_of("0123456789.f-e+") != string::npos)
 		return (0);
 	//check if exponent and error out if no numbers before or only numbers before
-	if(exp.find_first_of("eE") != string::npos)
+	if(exp.find_first_of("e") != string::npos)
 		not_exponent = 0;
 	//errpr out on multiple exponents or if no numbers either side of it
 	if(!not_exponent
-		&& (exp.find_first_of("0123456789") > exp.find_first_of("eE")
-		|| exp.find_last_of("0123456789+") < exp.find_first_of("eE")
-		|| exp.find_first_of("eE") != exp.find_last_of("eE")))
+		&& (exp.find_first_of("0123456789") > exp.find_first_of("e")
+		|| exp.find_last_of("0123456789+") < exp.find_first_of("e")
+		|| exp.find_first_of("e") != exp.find_last_of("e")))
 		return (0);
 	//error out on multiple - or numbers pre - or if exponent on multiple - after the first or if numbers before first
 	if (not_exponent && exp.find_first_of("-+") != string::npos
@@ -43,7 +44,8 @@ static int		valid_num_str(string &exp)
 	//error out if more neg or pos sines arround exponent than 1 behind number
 	if (!not_exponent && exp.find_first_of("-+", 1) != string::npos
 		&& (exp[exp.find_first_of("-+", 1) - 1] != 'e'
-		|| exp.find_first_of("-+", 1) != exp.find_last_of("-+")))
+		|| exp.find_first_of("-+", 1) != exp.find_last_of("-+")
+		|| exp.find_last_of("0123456789") < exp.find_first_of("-+")))
 		return (0);
 	// //error out on multiple . or dots before numbers with no precedent
 	if (exp.find_first_of('.') != string::npos
@@ -51,31 +53,33 @@ static int		valid_num_str(string &exp)
 		|| exp.find_first_of("0123456789") > exp.find('.')))
 		return (0);
 	//error out on multiple f or f not last or no decimal part
-	if (exp.find_first_of("fF") != string::npos
-		&& (exp.find_first_of(".eE") == string::npos
-		|| exp.find_first_of("fF") != exp.find_last_of("fF")
-		|| exp.find_last_of("0123456789") > exp.find_first_of("fF")))
+	if (exp.find_first_of("f") != string::npos
+		&& (exp.find_first_of(".e") == string::npos
+		|| exp.find_first_of("f") != exp.find_last_of("f")
+		|| exp.find_last_of("0123456789") > exp.find_first_of("f")))
 		return (0);
 	//if all conditions fail its a valid string
 	return (1);
 }
 
-static string	get_type(string &exp, double &value)
+static string	get_type(string &exp)
 {
 	//int neg = exp[0] == '-' ? 1 : 0;
-	char *end;
 	if (exp[0] == 0 || ((exp[0] > 0 && ((exp[0] < '0') || (exp[0] > '9')))
 			&& !exp[1]))
 		return ("char");
+	if (!exp.compare("-inff") || !exp.compare("inff") || !exp.compare("nanf"))
+		return ("float");
+	if (!exp.compare("-inf") || !exp.compare("inf") || !exp.compare("nan"))
+		return ("double");
 	if (!valid_num_str(exp))
 		return ("undefined");
-	if (exp.find_first_of(".eE") == string::npos)
+	if (exp.find_first_of(".e") == string::npos)
 		return ("int");
-	else if (exp.find_first_of("fF") == string::npos)
+	else if (exp.find_first_of("f") == string::npos)
 		return ("double");
 	else
 		return ("float");
-	value = strtod(exp.c_str(), &end);
 	return ("undefined");
 }
 
@@ -83,8 +87,6 @@ static string	get_type(string &exp, double &value)
 static void	format_str(string &exp)
 {
 	int	i;
-	// float b = 58.e5;
-	// b = 1000.0f;
 
 	i = 0;
 	while (exp[i] && (exp[i] == ' ' || exp[i] == '	'))
@@ -99,14 +101,30 @@ static void	format_str(string &exp)
 	while (i > 0 && (exp[i] == ' ' || exp[i] == '	'))
 		i--;
 	exp = exp.substr(0, i + 1);
+	for (i = 0; exp[i]; i++)
+		if (exp[i] >= 'A' && exp[i] <= 'Z')
+			exp[i] += 'a' - 'A';
 }
 
 void	ScalarConverter::converter(string &exp)
 {
-	double		value = 0;
+	// static string	types[4] = {"char", "int", "float", "double"};
+	// char	cValue;
+	// int		iValue;
+	float	fValue;
+	// double	dValue;
+
 	format_str(exp);
-	string	type = get_type(exp, value);
-	cout << exp << " = " << value <<endl;
+	string	type = get_type(exp);
+
+
+	cout << "float: ";
+	fValue = strtof(exp.c_str(), 0);
+	if (errno == ERANGE)
+		cout << "Impossible";
+	else
+		cout << fValue;
+	cout << endl;
 	cout <<  type << endl;
 	
 }
