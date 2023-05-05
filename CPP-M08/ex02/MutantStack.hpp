b0b636c3f605973e25f4fd8f336f1b742afd4476
LeoMainini <leonardo.apple.mainini@gmail.com>
2023-05-05 21:03:02 +0100

    add begin and end function and iterator type to MutantStack class

--- CPP-M08/ex02/MutantStack.hpp
+++ CPP-M08/ex02/MutantStack.hpp
@@ -5,24 +5,13 @@
 #include <iostream>
 #include <deque>
 
-template <class T>
-class MutantStack: public std::stack<T, std::deque<T> >
+template <class T, class C=std::deque<T> >
+class MutantStack: public std::stack<T, C>
 {
-	protected:
-		typedef std::stack<T, std::deque<T> > base_s;
 	public:
-		typedef typename base_s::value_type value_type;
-		typedef typename base_s::container_type container_type;
-		typedef typename base_s::size_type size_type;
-
-		using base_s::empty;
-		using base_s::size;
-		using base_s::top;
-		using base_s::push;
-		// using base_s::emplace;
-		// using base_s::swap;
-		using base_s::pop;
-
+		typedef typename C::iterator iterator;
+		iterator begin() { return (this->c.begin()); }
+		iterator end() { return (this->c.end()); }
 };
 
 #endif
--- CPP-M08/ex02/main.cpp
+++ CPP-M08/ex02/main.cpp
@@ -5,5 +5,38 @@ int main(void)
 	MutantStack<int> a;
 
 	a.push(5);
-	// a.begin();
+	a.push(1);
+	a.push(2);
+	a.push(3);
+	a.push(-1);
+	a.push(1);
+	a.push(8);
+
+	a.begin();
+	for(MutantStack<int>::iterator it = a.begin(); it != a.end(); it++)
+		std::cout << *it << std::endl;
+	{
+		MutantStack<int> mstack;
+		mstack.push(5);
+		mstack.push(17);
+		std::cout << mstack.top() << std::endl;
+		mstack.pop();
+		std::cout << mstack.size() << std::endl;
+		mstack.push(3);
+		mstack.push(5);
+		mstack.push(737);
+		//[...]
+		mstack.push(0);
+		MutantStack<int>::iterator it = mstack.begin();
+		MutantStack<int>::iterator ite = mstack.end();
+		++it;
+		--it;
+		while (it != ite)
+		{
+		std::cout << *it << std::endl;
+		++it;
+		}
+		std::stack<int> s(mstack);
+		return 0;
+	}
 }
