8357fa8cc9591aa9ecc682011a0d222284712f2e
LeoMainini <leonardo.apple.mainini@gmail.com>
2023-05-17 20:47:48 +0100

    complete modern merge-insert sort implementation for both list and vectors - implement pseudo std::next-prev from cpp11 in cpp98

--- CPP-M09/ex02/PmergeMe.hpp
+++ CPP-M09/ex02/PmergeMe.hpp
@@ -17,26 +17,10 @@ class PmergeMe {
 		void	insertSort(std::list<int> &a);
 		void	divideVectorPairs();
 		void	divideListPairs();
-		void	insertSortVectorPairs();
+		void	insertSortListPairs();
 		void	divideContainer(std::list<int> &a, std::list<int> &b);
 		void	sortVector();
 		void	sortList();
-		template <class T>
-		void	printContainer(const T &c)
-		{
-			typedef typename T::const_iterator citer;
-			size_t	i = 0;
-			citer it;
-
-			for (it = c.begin() ; it != c.end(); it++, i++)
-				std::cout << *it << " ";
-			if (c.size() > 5)
-				std::cout << "[...]";
-			else if (it != c.end())
-				std::cout << *it;
-			std::cout << std::endl;
-
-		}
 
 	public:
 		int		init(char **numberList);
@@ -50,5 +34,61 @@ class PmergeMe {
 		~PmergeMe();
 
 };
+
+template <class T>
+void	printContainer(const T &c)
+{
+	typedef typename T::const_iterator citer;
+	size_t	i = 0;
+	citer it;
+
+	for (it = c.begin() ; it != c.end() && i < 5; it++, i++)
+		std::cout << *it << " ";
+	if (c.size() > 5)
+		std::cout << "[...]";
+	else if (it != c.end())
+		std::cout << *it;
+	std::cout << std::endl;
+}
+
+template <class T>
+typename T::iterator nextIt(T &o, typename T::iterator it)
+{
+	typedef typename T::iterator iter;
+	iter tmp = it;
+	if (tmp == o.end() || ++tmp == o.end())
+		return (o.end());
+	return (tmp);
+}
+
+template <class T>
+typename T::iterator prevIt(T &o, typename T::iterator it)
+{
+	typedef typename T::iterator iter;
+	iter tmp = it;
+	if (tmp == o.end())
+	{
+		for (iter tmp2 = o.begin(); tmp2 != o.end() ; tmp2++)
+			tmp = tmp2;
+		return (tmp);
+	}
+	if (tmp == o.begin())
+		return (o.end());
+	return (--tmp);
+}
+
+template <class T, class C>
+void	insertSubContainerPairs(T &c, C &sc)
+{
+	typedef typename C::iterator subIter;
+
+	for (subIter i = sc.begin(); i != sc.end(); i++)
+	{
+		c.insert(c.begin(), (*i).first);
+		if ((*i).second == -1)
+			continue ;
+		c.insert(std::lower_bound(c.begin(), c.end(), (*i).second), (*i).second);
+	}
+}
 
 #endif
